b79539e382aca8685c488bccbf5928324aaa6e72
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-04-30 09:07:58 +0530

    Time: 49 ms (69.77%), Space: 47.8 MB (99.14%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    var i = 0
+    for (i; i<args.length;i++) {
+        
+    }
+    return i
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
