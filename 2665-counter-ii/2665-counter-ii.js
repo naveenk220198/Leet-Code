c3951dea749c563c38acbbda88e5515407caf84e
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-04-30 07:39:14 +0530

    Time: 57 ms (67.27%), Space: 50.8 MB (97.62%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    initial = init
+    curr = init
+    return {
+        increment: function() {
+            return ++curr
+        },
+        reset: function() {
+            curr = initial
+            return curr
+        },
+        decrement: function() {
+            return --curr
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
