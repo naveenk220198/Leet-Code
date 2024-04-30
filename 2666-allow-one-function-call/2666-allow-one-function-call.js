612aa10a5a62e48f68b78b41a2ffb81d0d0feb11
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-04-30 09:42:53 +0530

    Time: 42 ms (95.86%), Space: 48.8 MB (43.08%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+
+var once = function(fn) {
+    let called = false
+    let result = 0
+    return function(...args){
+        if (!called) {
+            result = fn(...args)
+            called = true
+        }
+        else { 
+            return undefined
+        }
+        return result
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
