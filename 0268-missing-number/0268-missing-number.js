2ec236663e7a4c9cd7bd7d9db0386b6eba1c29fe
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-05-12 11:02:58 +0530

    Time: 58 ms (65.64%), Space: 51.1 MB (77.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,25 +4,34 @@
  */
 var missingNumber = function(nums) {
     l = nums.length
-    if (l==1) {
-        if (nums[0] == 1) {
-            return 0
-        }
-        else {
-            return 1
+    // if (l==1) {
+    //     if (nums[0] == 1) {
+    //         return 0
+    //     }
+    //     else {
+    //         return 1
+    //     }
+    // }
+    // for (let i = 1; i <= l; i++) {
+    //     found = 0
+    //     for (let j = 0; j < l; j++) {
+    //         if (nums[j] == i) {
+    //             found = 1
+    //             break
+    //         }
+    //     }
+    //     if (found == 0) {
+    //         return i
+    //     }
+    // }
+    // return 0
+    let hash = new Array(l+1).fill(0)
+    for (let j=0; j< l; j++) {
+        hash[nums[j]]++
+    }
+    for (let j = 0; j < hash.length; j++) {
+        if (hash[j]==0) {
+            return j
         }
     }
-    for (let i = 1; i <= l; i++) {
-        found = 0
-        for (let j = 0; j < l; j++) {
-            if (nums[j] == i) {
-                found = 1
-                break
-            }
-        }
-        if (found == 0) {
-            return i
-        }
-    }
-    return 0
 };
