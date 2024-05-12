be4329608dca44b589b01937515ca8ecb111084f
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-05-12 12:10:45 +0530

    Time: 70 ms (34.99%), Space: 55.2 MB (12.58%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,21 +4,35 @@
  */
 var singleNumber = function(nums) {
 //     Brute
-    l = nums.length
-    if (l==1) {
-        return nums[0]
+    // l = nums.length
+    // if (l==1) {
+    //     return nums[0]
+    // }
+    // for (let i=0 ; i<l; i++) {
+    //     let cnt = 0
+    //     // const num = nums[i]
+    //     for(let j=0; j<l; j++){
+    //         if (nums[j]===nums[i]) {
+    //             cnt++
+    //             // console.log(cnt)
+    //         }
+    //     }
+    //     if (cnt === 1) {
+    //         return nums[i]
+    //     }
+    // }
+    
+    
+//     Better 
+    let l = nums.length
+    let m = new Map()
+    for (let i = 0; i< l; i++) {
+        let num = nums[i]
+        m.set(num, (m.get(num) || 0) + 1)
     }
-    for (let i=0 ; i<l; i++) {
-        let cnt = 0
-        // const num = nums[i]
-        for(let j=0; j<l; j++){
-            if (nums[j]===nums[i]) {
-                cnt++
-                // console.log(cnt)
-            }
-        }
-        if (cnt === 1) {
-            return nums[i]
+    for (let [num, count] of m){
+        if (count == 1) {
+            return num
         }
     }
     
