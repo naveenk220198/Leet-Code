fa2b97ba44b7a510e6a9307123bcf05370cb6228
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-05-12 17:25:27 +0530

    Time: 54 ms (50.70%), Space: 49.2 MB (30.00%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -4,35 +4,63 @@
  */
 var sortColors = function(nums) {
 //     Better - > get counts of all colors and modify given array
-    let c_r = 0
-    let c_b = 0
-    let c_w = 0
-    for (let i = 0; i< nums.length; i++) {
-        if (nums[i] == 0) {
-            c_r++
+    // let c_r = 0
+    // let c_b = 0
+    // let c_w = 0
+    // for (let i = 0; i< nums.length; i++) {
+    //     if (nums[i] == 0) {
+    //         c_r++
+    //     }
+    //     if (nums[i] == 1) {
+    //         c_w++
+    //     }
+    //     else {
+    //         c_b++
+    //     }
+    // }
+    // for (let i = 0; i< nums.length; i++) {
+    //     if (c_r) {
+    //         nums[i] = 0
+    //         c_r --
+    //         continue
+    //     }
+    //     if (c_w) {
+    //         nums[i] = 1
+    //         c_w--
+    //         continue
+    //     }
+    //     if (c_b) {
+    //         nums[i] = 2
+    //         c_b--
+    //         continue
+    //     }
+    // }
+    
+    
+//     Optimal  - National flag algo, 3 pointer
+    let left = 0
+    let mid = 0
+    let high = nums.length - 1
+    let temp
+    while(mid <= high) {
+        if (nums[mid] == 0) {
+            temp = nums[mid]
+            nums[mid] = nums[left]
+            nums[left] = temp
+            mid++
+            left++
         }
-        if (nums[i] == 1) {
-            c_w++
+        else if(nums[mid] == 1) {
+            mid++
+           
         }
         else {
-            c_b++
+            temp = nums[mid]
+            nums[mid] = nums[high]
+            nums[high] = temp
+            high--
         }
     }
-    for (let i = 0; i< nums.length; i++) {
-        if (c_r) {
-            nums[i] = 0
-            c_r --
-            continue
-        }
-        if (c_w) {
-            nums[i] = 1
-            c_w--
-            continue
-        }
-        if (c_b) {
-            nums[i] = 2
-            c_b--
-            continue
-        }
-    }
+
+
 };
