791eb575cb879e68a816fbb5193d58034ea49c56
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-05-12 18:54:34 +0530

    Time: 66 ms (32.84%), Space: 51.6 MB (54.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,18 +4,34 @@
  */
 var majorityElement = function(nums) {
     let l = nums.length
-    let maj = Math.floor(nums.length / 2)
-    let cnt = 0
-    for (let i = 0; i<l; i++) {
-        cnt = 0
-        for (let j = 0; j<l; j++) {
-            if (nums[j]==nums[i]) {
-                cnt++
+//     let maj = Math.floor(nums.length / 2)
+//     let cnt = 0
+//     for (let i = 0; i<l; i++) {
+//         cnt = 0
+//         for (let j = 0; j<l; j++) {
+//             if (nums[j]==nums[i]) {
+//                 cnt++
                
-            }
+//             }
+//         }
+//         if (cnt > maj) {
+//             return nums[i]
+//         }
+//     }
+//     return -1
+    let hm = new Map()
+    for (let i = 0; i< l; i++) {
+        if (hm.has(nums[i])) {
+            
+            hm.set(nums[i], hm.get(nums[i]||0) + 1)
         }
-        if (cnt > maj) {
-            return nums[i]
+        else {
+            hm.set(nums[i], 1)
+        }
+    }
+    for (const [num, count] of hm) {
+        if (count > Math.floor(l/2) ) {
+            return num
         }
     }
 };
