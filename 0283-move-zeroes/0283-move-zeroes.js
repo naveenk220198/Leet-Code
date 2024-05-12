7c3bf821f75840ce1c6cf93b8a4f1e1bf94116ca
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-05-12 08:14:56 +0530

    Time: 78 ms (48.79%), Space: 53.4 MB (90.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,18 +4,35 @@
  */
 
 var moveZeroes = function(nums) {
-    ans = []
+    // ans = []
     l = nums.length
-    for (let i = 0; i < l; i++) {
-        if (nums[i] !== 0) {
-            ans.push(nums[i])
+    // for (let i = 0; i < l; i++) {
+    //     if (nums[i] !== 0) {
+    //         ans.push(nums[i])
+    //     }
+    // }
+    // tl = ans.length
+    // for (let i =tl; i< l; i++) {
+    //     ans.push(0)
+    // }
+    // for (let i = 0; i<l; i++) {
+    //     nums[i] = ans[i]
+    // }
+    let j
+    for (let i = 0; i<l ; i++) {
+        if (nums[i] == 0) {
+            j = i
+            break
         }
     }
-    tl = ans.length
-    for (let i =tl; i< l; i++) {
-        ans.push(0)
+    for (let i = j+1; i<l; i++) {
+        if (nums[i] != 0) {
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            j++
+        }
     }
-    for (let i = 0; i<l; i++) {
-        nums[i] = ans[i]
-    }
+    
+    
 };
