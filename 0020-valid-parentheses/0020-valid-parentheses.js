1ce61fcd8043fd20fcd3a476ce81360521cf20c7
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-07-02 19:18:29 +0530

    Time: 61 ms (40.90%), Space: 49.6 MB (79.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,26 +3,54 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    // let stack = []
+    // for (let c=0; c<s.length; c++) {
+    //     if (s[c] === '(' || s[c] === '{' || s[c] === '[') {
+    //         stack.push(s[c])
+    //     }
+    //     else {
+    //         if (stack.length === 0) {
+    //             return false
+    //         }
+    //         let close = s[c]
+    //         let ele = stack.pop()
+    //         if ((ele === "(" && close === ")") || (ele === "[" && close === "]") || (ele === "{" &&              close === "}")) {
+    //             continue
+    //         }
+    //         else {
+    //             return false
+    //         }
+    //     }
+    // }
+    // if (stack.length != 0) {
+    //     return false
+    // }
+    // return true
+    
     let stack = []
-    for (let c=0; c<s.length; c++) {
-        if (s[c] === '(' || s[c] === '{' || s[c] === '[') {
+    for ( let c = 0; c < s.length; c++) {
+        if (s[c] === "(" || s[c] === '[' || s[c] === '{') {
             stack.push(s[c])
         }
         else {
             if (stack.length === 0) {
                 return false
             }
-            let close = s[c]
-            let ele = stack.pop()
-            if ((ele === "(" && close === ")") || (ele === "[" && close === "]") || (ele === "{" &&              close === "}")) {
-                continue
-            }
             else {
-                return false
+                let close = s[c]
+                let open = stack.pop()
+                if (open === '(' && close === ')' || open === '[' && close === ']' || open === '{' && close === '}') {
+                    
+                    continue
+                }
+                else {
+                    return false
+                }
+                
             }
         }
     }
-    if (stack.length != 0) {
+    if (stack.length) {
         return false
     }
     return true
