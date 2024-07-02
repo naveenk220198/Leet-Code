e4a58b95849bede230f145a3a5c70f717c738c3e
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-07-02 20:41:37 +0530

    Time: 64 ms (47.64%), Space: 52.2 MB (32.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,21 +4,60 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let stack = []
-    let res = new Array(nums1.length).fill(-1)   
-    let hashMap = new Map()
-    let n = nums2.length
-    for (let i=0; i<n; i++) {
-        while (stack.length !== 0 && stack[stack.length - 1] < nums2[i]) {
-            const popped = stack.pop();
-            hashMap.set(popped, nums2[i])
+    // let stack = []
+    // let res = new Array(nums1.length).fill(-1)   
+    // let hashMap = new Map()
+    // let n = nums2.length
+    // for (let i=0; i<n; i++) {
+    //     while (stack.length !== 0 && stack[stack.length - 1] < nums2[i]) {
+    //         const popped = stack.pop();
+    //         hashMap.set(popped, nums2[i])
+    //     }
+    //     stack.push(nums2[i])
+    // }
+    // for (let i=0; i<nums1.length; i++) {
+    //     if (hashMap.has(nums1[i])) {
+    //         res[i] = hashMap.get(nums1[i])
+    //     }
+    // }
+    // return res
+
+    let stack = [];
+    let res = new Array(nums2.length).fill(-1); // Initialize result array with -1
+    let res2 = []
+    let n = nums2.length;
+
+    for (let i = n - 1; i >= 0; i--) {
+        // while (stack.length !== 0 && nums1[i % n] > nums2[stack[stack.length - 1]]) {
+        //     const popped = stack.pop();
+        //     res[popped] = nums2[i % n];
+        // }
+        // if (i < n) {
+        //     stack.push(i);
+        // }
+        if (stack.length === 0) {
+            res[i] = -1
+            stack.push(nums2[i])
         }
-        stack.push(nums2[i])
-    }
-    for (let i=0; i<nums1.length; i++) {
-        if (hashMap.has(nums1[i])) {
-            res[i] = hashMap.get(nums1[i])
+        else {
+            while (stack[stack.length - 1] < nums2[i]) {
+                stack.pop()
+            }
+            if (stack.length) {
+                res[i] = stack[stack.length - 1]
+                stack.push(nums2[i])
+            }
+            else {
+                res[i] = -1
+                stack.push(nums2[i])
+            }
         }
+        
     }
-    return res
+    for (let i = 0; i < nums1.length; i++) {
+        let ele = nums1[i]
+        let index = nums2.indexOf(ele)
+        res2[i] = res[index]
+    }
+    return res2;
 };
