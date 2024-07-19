aa75a61cf7d520c623413de589f76e3c51e3d0e3
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-07-19 18:46:10 +0530

    Time: 50 ms (94.32%), Space: 52.1 MB (10.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,73 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    res = []
-    for (let i = 0; i< nums.length; i++) {
-        for (let j = i+1; j<nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                res.push(i)
-                res.push(j)
-                return res
-            }
+    let hm = new Map()
+    let summ = 0
+    for (let i = 0; i < nums.length; i++) {
+        summ = nums[i]
+        prevSum =  target - summ
+        if (hm.has(prevSum)){
+            return [hm.get(prevSum), i]
+        }
+        else {
+            hm.set(nums[i], i)
         }
     }
+    return [-1, -1]
+
+
+
+
+
+
+
+
+
+//     res = []
+//     for (let i = 0; i< nums.length; i++) {
+//         for (let j = i+1; j<nums.length; j++) {
+//             if (nums[i] + nums[j] == target) {
+//                 res.push(i)
+//                 res.push(j)
+//                 return res
+//             }
+//         }
+//     }
+    
+// //     Better
+//     let res= []
+//     let hm = new Map()
+//     let num = 0
+//     for (let i = 0; i< nums.length; i++) {
+//         num = nums[i]
+//         rem = target - num
+//         if (hm.has(rem)) {
+//             res.push(hm.get(rem))
+//             res.push(i)
+//             return res
+//         }
+//         else {
+//             hm.set(num, i)
+//         }    
+//     }
+//     return res
+// //     Optimal
+
+//     nums.sort((a, b) => a - b);
+
+//     let left = 0;
+//     let right = nums.length - 1;
+
+//     while (left < right) {
+//         const sum = nums[left] + nums[right];
+
+//         if (sum === target) {
+//             return [nums[left], nums[right]];
+//         } else if (sum < target) {
+//             left++;
+//         } else {
+//             right--;
+//         }
+//     }
+//     return [];
 };
