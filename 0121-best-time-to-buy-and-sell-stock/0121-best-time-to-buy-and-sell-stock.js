26cd9a43ebf8cb9a81ec4b367e117530bd4de967
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2024-07-20 18:10:40 +0530

    Time: 78 ms (40.76%), Space: 58.9 MB (56.00%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,7 +3,23 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-//     Brute
+//     let maxi = 0
+//     for(let i = 0; i < prices.length; i++) {
+//         for(let j =i+1; j < prices.length; j++) {
+//             maxi = Math.max(maxi, prices[j] - prices[i])
+//         }
+        
+//     }
+//     return maxi
+
+// Optimal
+    let minStock = Number.MAX_SAFE_INTEGER
+    let maxP = 0
+    for (let i = 0; i < prices.length; i++) {
+        minStock = Math.min(minStock, prices[i])
+        maxP = Math.max(maxP, prices[i] - minStock)
+    }
+    return maxP
     // let maxp = 0
     // for (i=0; i < prices.length ; i++) {
     //     for (j=i+1; j < prices.length; j++) {
@@ -13,11 +29,11 @@ var maxProfit = function(prices) {
     // return maxp
     
 //     Optimal
-    let maxp = 0
-    let minp = Number.MAX_SAFE_INTEGER
-    for (i=0; i<prices.length; i++) {
-        minp = Math.min(minp, prices[i])
-        maxp = Math.max(maxp, prices[i] - minp)
-    }
-    return maxp
+    // let maxp = 0
+    // let minp = Number.MAX_SAFE_INTEGER
+    // for (i=0; i<prices.length; i++) {
+    //     minp = Math.min(minp, prices[i])
+    //     maxp = Math.max(maxp, prices[i] - minp)
+    // }
+    // return maxp
 };
