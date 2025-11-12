4b1267a58dca183300e569b6291c9dee2ddad2ee
naveenk220198 <143642156+naveenk220198@users.noreply.github.com>
2025-11-12 21:01:04 +0530

    Time: 4 ms (45.45%), Space: 59.1 MB (31.97%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    let i = 0
+    let res = []
+    let n = intervals.length
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        res.push(intervals[i])
+        i++
+    }
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0])
+         newInterval[1] = Math.max(intervals[i][1], newInterval[1])
+         i++
+    }
+    res.push(newInterval)
+    while (i < n) {
+        res.push(intervals[i])
+        i++
+    }
+    return res
+};
